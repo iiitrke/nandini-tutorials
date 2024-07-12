78e88b339060d405b5afb2e5f3640c782b5fed73
Itasca Technologies <info@itasca.co.in>
2024-07-13 00:06:54 +0530

    sdfdsf

---
+++ first/src/AltoCar.java
@@ -0,0 +1,3 @@
+public class AltoCar implements  ICar{
+
+}
---
+++ first/src/Halix.java
@@ -0,0 +1,12 @@
+public class Halix implements  ICar{
+
+    @Override
+    public void start() {
+       System.out.println("Started");
+    }
+
+    public void jump(){
+        System.out.println("JUMp");
+    }
+
+}
